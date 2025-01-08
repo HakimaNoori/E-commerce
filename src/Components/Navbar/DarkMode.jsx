f383e682999dc9b02b3908fcfe9c40a9600ea105
HakimaNoori <hakima.noori.1379@gmail.com>
2025-01-08 11:29:43 +0430

    Add Hero file  this project.

--- src/Components/Navbar/DarkMode.jsx
+++ src/Components/Navbar/DarkMode.jsx
@@ -20,12 +20,12 @@ const DarkMode = () => {
       <div className='relative'>
           <img src={LightButton} alt=""
               onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-              className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgbe(0,0,0,0.1)] translate-all duration-300 absolute right-0 z-10 ${theme === "dark" ? "opacity-0" : "opacity-100"
-              }`}
+              className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgbe(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${theme === "dark" ? "opacity-0" : "opacity-100"
+              }`} 
           />
           <img src={DarkButton} alt=""
               onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-              className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgbe(0,0,0,0.1)] translate-all duration-300" />
+              className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgbe(0,0,0,0.1)] transition-all    duration-300" />
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Logo from "../../assets/logo.png";
 import { IoMdSearch } from "react-icons/io";
-import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown } from 'react-icons/fa6';
 import DarkMode from './DarkMode';
-import { data } from 'autoprefixer';
+// import { data } from 'autoprefixer';
 
 const Menu = [
     {
@@ -32,11 +33,30 @@ const Menu = [
         link: "/#",
     },
 ];
+
+const DropdownLinks = [
+    {
+        id: 1,
+        name: "Trending Products",
+        link: "/#",
+    },
+    {
+         id:2,
+        name: " Beas Selling",
+        link: "/#",
+    },
+    {
+         id: 3,
+        name: "Top Rated",
+        link: "/#",
+    },
+  
+]
 const Navbar = () => {
   return (
       <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
           {/* upper Navbar */}
-          <div className='bg-primary/40 py-3'>
+          <div className='bg-primary/40 py-2 '>
               <div className='container flex justify-between items-center'>
                   <div>
                       <a href="#" className='font-bold text-2xl sm:text-3xl flex gap-2'>
@@ -47,29 +67,30 @@ const Navbar = () => {
                   {/* search */}
                   <div className='flex justify-between items-center gap-4'>
                       <div className='relative group hidden sm:block'>
-                          <input type="text" placeholder='search' className='w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary
+                          <input
+                              type="text" placeholder='search' className='w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary
                           dark:border-gray-500
                           dark:bg-gray-800' />
                           <IoMdSearch
                           className='text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3'/>
                       </div>
-                  </div>
-                  {/* order button */}
+                       {/* order button */}
                   <button
-                      onClick={() => alert("Ordering not available yet")}
-                      className='bg-gradient-to-r from-primary to-secondary translate-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group'>
+                      onClick={() => handleOrderPopup()}
+                      className='bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group'>
                       <span
                           className='group-hover:block hidden translate-all duration-200'>
                           Order
                       </span>
                       <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer"/>
-                  </button>
-                  {/* Darkmode Switch */}
+                  </button> 
+                     {/* Darkmode */}
                   <div className="">
                       < DarkMode/>
                   </div>
               </div>
-          </div>
+              </div>
+              </div>
           {/* lower Navbar */}
           <div className='flex justify-center'>
               <ul className='sm:flex hidden items-center gap-4'>
@@ -83,9 +104,10 @@ const Navbar = () => {
                           </a>
                           </li>
                   ))}
-                  {/* simple Dropdown and links */}
+                  {/* simple Dropdown and links */} 
                   <li className='group relative cursor-pointer'>
-                      <a href="#" className="flex items-center gap-[2px] py-2">
+                      <a href="#"
+                          className="flex items-center gap-[2px] py-2">
                           Trending Products
                           <span>
                               <FaCaretDown
